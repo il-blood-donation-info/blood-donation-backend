2d625f99337184b9d90b3b80c0902fc513498f9e
Moti Asayag <masayag@redhat.com>
2023-10-16 01:27:52 +0300

    Implement user actions

--- bloodinfo/server.go
+++ bloodinfo/server.go
@@ -34,19 +34,64 @@ func (s StrictBloodInfoServer) UpdateStation(ctx context.Context, request Update
 
 // GetUsers get all users
 func (s StrictBloodInfoServer) GetUsers(ctx context.Context, request GetUsersRequestObject) (GetUsersResponseObject, error) {
-	panic("implement me")
+	var users []User
+	tx := s.db.Find(&users)
+	if tx.Error != nil {
+		return GetUsers500JSONResponse{
+			Message: fmt.Sprintf("error getting users: %w", tx.Error),
+		}, nil
+	}
+	return GetUsers200JSONResponse(users), nil
 }
 
 // CreateUser creates user
 func (s StrictBloodInfoServer) CreateUser(ctx context.Context, request CreateUserRequestObject) (CreateUserResponseObject, error) {
-	panic("implement me")
+	// TODO: validate request is authorized only by admin role
+	user := User{
+		FirstName:   request.Body.FirstName,
+		LastName:    request.Body.LastName,
+		Email:       request.Body.Email,
+		Description: request.Body.Description,
+		Phone:       request.Body.Phone,
+		Role:        request.Body.Role,
+	}
+	tx := s.db.Create(&user)
+
+	if tx.Error != nil {
+		return CreateUser500JSONResponse{
+			Message: fmt.Sprintf("error creating user: %w", tx.Error),
+		}, nil
+	}
+
+	return CreateUser201JSONResponse(user), nil
 }
 
 // DeleteUser deletes user
 func (s StrictBloodInfoServer) DeleteUser(ctx context.Context, request DeleteUserRequestObject) (DeleteUserResponseObject, error) {
-	panic("implement me")
+	user := User{
+		Id: request.Id,
+	}
+	tx := s.db.Delete(&user)
+	if tx.Error != nil {
+		return DeleteUser500JSONResponse{
+			Message: fmt.Sprintf("error deleting user: %w", tx.Error),
+		}, nil
+	}
+	return DeleteUser200Response{}, nil
 }
 
 func (s StrictBloodInfoServer) UpdateUser(ctx context.Context, request UpdateUserRequestObject) (UpdateUserResponseObject, error) {
-	panic("implement me")
+	tx := s.db.Model(&User{}).Where("id = ?", request.Id).Updates(User{
+		FirstName:   request.Body.FirstName,
+		LastName:    request.Body.LastName,
+		Email:       request.Body.Email,
+		Description: request.Body.Description,
+		Phone:       request.Body.Phone,
+	})
+	if tx.Error != nil {
+		return UpdateUser500JSONResponse{
+			Message: fmt.Sprintf("error updating user: %w", tx.Error),
+		}, nil
+	}
+	return UpdateUser200JSONResponse{}, nil
 }
