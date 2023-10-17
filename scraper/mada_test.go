84be0c5270300b21032d4e43165ff200ae6396ec
Ian SEBBAGH <ianouf@gmail.com>
2023-10-17 14:24:11 +0300

    Testing scraper to DB with new models

--- scraper/mada_test.go
+++ scraper/mada_test.go
@@ -108,3 +108,25 @@ func TestScrapeMada(t *testing.T) {
 
 	//SaveData(madaResponse)
 }
+
+func TestScraper2DB(t *testing.T) {
+	madaResponse, err := ScrapeMada()
+	if err != nil {
+		t.Fatalf("Failed to scrape Mada: %s", err)
+	}
+
+	for _, result := range madaResponse {
+		fmt.Printf("Name: %s\n", result.Name)
+		fmt.Printf("Address: %s %s %s\n", result.City, result.Street, result.NumHouse)
+		fmt.Printf("Open Time: %s\n", result.FromHour)
+		fmt.Printf("Close Time: %s\n\n", result.ToHour)
+		fmt.Printf("Datetime : %s\n\n", result.DateDonation)
+	}
+
+	// Basic check if we got some data
+	if len(madaResponse) == 0 {
+		t.Fatal("Received empty response from Mada")
+	}
+
+	SaveData(madaResponse)
+}
