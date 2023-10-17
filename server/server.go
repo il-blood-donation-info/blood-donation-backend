62268c69d0b707fac09182c5638bd11118c64626
Moti Asayag <masayag@redhat.com>
2023-10-18 00:26:22 +0300

    Add HTTPS support + refactor
    The PR does some refactoring to the code to make it better organized:
    - Move api related files and generated code to api/ directory
    - Move server code to /server directory
    
    It also enables HTTPS to interact with the server and offers a tool to
    generate self-signed certificate.

---
+++ server/server.go
@@ -0,0 +1,98 @@
+package server
+
+import (
+	"blood-donation-backend/api"
+	"context"
+	"fmt"
+	"gorm.io/gorm"
+)
+
+// StrictBloodInfoServer implements StrictServerInterface
+type StrictBloodInfoServer struct {
+	db *gorm.DB
+}
+
+func NewStrictBloodInfoServer(db *gorm.DB) StrictBloodInfoServer {
+	return StrictBloodInfoServer{db: db}
+}
+
+// GetStations gets all stations
+func (s StrictBloodInfoServer) GetStations(ctx context.Context, request api.GetStationsRequestObject) (api.GetStationsResponseObject, error) {
+	var stations []api.Station
+	tx := s.db.Find(&stations)
+	if tx.Error != nil {
+		return api.GetStations500JSONResponse{
+			Message: fmt.Sprintf("error getting stations: %v", tx.Error),
+		}, nil
+	}
+	return api.GetStations200JSONResponse(stations), nil
+}
+
+// UpdateStation updates station
+func (s StrictBloodInfoServer) UpdateStation(ctx context.Context, request api.UpdateStationRequestObject) (api.UpdateStationResponseObject, error) {
+	panic("implement me")
+}
+
+// GetUsers get all users
+func (s StrictBloodInfoServer) GetUsers(ctx context.Context, request api.GetUsersRequestObject) (api.GetUsersResponseObject, error) {
+	var users []api.User
+	tx := s.db.Find(&users)
+	if tx.Error != nil {
+		return api.GetUsers500JSONResponse{
+			Message: fmt.Sprintf("error getting users: %v", tx.Error),
+		}, nil
+	}
+	return api.GetUsers200JSONResponse(users), nil
+}
+
+// CreateUser creates user
+func (s StrictBloodInfoServer) CreateUser(ctx context.Context, request api.CreateUserRequestObject) (api.CreateUserResponseObject, error) {
+	// TODO: validate request is authorized only by admin role
+	user := api.User{
+		FirstName:   request.Body.FirstName,
+		LastName:    request.Body.LastName,
+		Email:       request.Body.Email,
+		Description: request.Body.Description,
+		Phone:       request.Body.Phone,
+		Role:        request.Body.Role,
+	}
+	tx := s.db.Create(&user)
+
+	if tx.Error != nil {
+		return api.CreateUser500JSONResponse{
+			Message: fmt.Sprintf("error creating user: %v", tx.Error),
+		}, nil
+	}
+
+	return api.CreateUser201JSONResponse(user), nil
+}
+
+// DeleteUser deletes user
+func (s StrictBloodInfoServer) DeleteUser(ctx context.Context, request api.DeleteUserRequestObject) (api.DeleteUserResponseObject, error) {
+	user := api.User{
+		Id: request.Id,
+	}
+	tx := s.db.Delete(&user)
+	if tx.Error != nil {
+		return api.DeleteUser500JSONResponse{
+			Message: fmt.Sprintf("error deleting user: %v", tx.Error),
+		}, nil
+	}
+	return api.DeleteUser200Response{}, nil
+}
+
+func (s StrictBloodInfoServer) UpdateUser(ctx context.Context, request api.UpdateUserRequestObject) (api.UpdateUserResponseObject, error) {
+	tx := s.db.Model(&api.User{}).Where("id = ?", request.Id).Updates(api.User{
+		FirstName:   request.Body.FirstName,
+		LastName:    request.Body.LastName,
+		Email:       request.Body.Email,
+		Description: request.Body.Description,
+		Phone:       request.Body.Phone,
+	})
+	if tx.Error != nil {
+		return api.UpdateUser500JSONResponse{
+			Message: fmt.Sprintf("error updating user: %v", tx.Error),
+		}, nil
+	}
+	return api.UpdateUser200JSONResponse{}, nil
+}
